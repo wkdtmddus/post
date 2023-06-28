5dbecec316a4e31f8c683b22c274bee27b779e8e
Robbie <Robbie@email.com>
2023-06-28 22:16:23 +0900

    SpringBoot JPA

--- src/main/java/com/inoc/post/entity/Post.java
+++ src/main/java/com/inoc/post/entity/Post.java
@@ -1,18 +1,26 @@
 package com.inoc.post.entity;
 
 import com.inoc.post.dto.PostRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@Table(name = "post")
 @NoArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "author", nullable = false)
     private String author;
     private String password;
+    @Column(name = "title", nullable = false)
     private String title;
+    @Column(name = "content", nullable = false, length = 500)
     private String content;
 
     public Post(PostRequestDto requestDto) {
